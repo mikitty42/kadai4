f99be21759ae905089262046e7656296979e3cbd
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-05-05 20:14:31 +0900

    kadai

---
+++ test.rb
@@ -0,0 +1,13 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/miki.cgi', WEBrick::HTTPServlet::CGIHandler, 'miki.rb')
+server.start
